e7b4a0fa491ba52a66915ca8867be4a3f7bca9cb
Evan Sangaline <esangaline@gmail.com>
2018-08-06 12:25:37 -0400

    Add a webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
+const mode = process.env.NODE_ENV || 'development';
+
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'user-agents',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  target: 'node',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          mangle: {
+            reserved: [
+              'Slice',
+              'SliceArray',
+              'SliceString',
+            ],
+          },
+        },
+      }),
+    ],
+  },
+  mode,
+};
