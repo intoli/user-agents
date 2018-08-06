36dc07ef85ebb68816571e2c0d16cb6eb1323f0a
Evan Sangaline <esangaline@gmail.com>
2018-08-06 13:45:23 -0400

    Implement the `random()` method.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
-const random = () => {
-  const userAgent = 'TODO: implement this.';
-  return userAgent;
+import userAgents from './user-agents.json';
+
+
+// Only compute the normalized tiers once for efficiency.
+let normalizedTiers;
+const getNormalizedTiers = () => {
+  if (normalizedTiers) {
+    return normalizedTiers;
+  }
+  const numberOfUserAgents = userAgents.length;
+  const marketShares = userAgents.map(userAgent => userAgent.marketShare);
+  const totalMarketShare = marketShares.reduce((total, marketShare) => total + marketShare);
+  normalizedTiers = [];
+  marketShares.forEach((marketShare, index) => {
+    const runningSum = index > 0 ? normalizedTiers[index - 1] : 0;
+    normalizedTiers.push((marketShare / totalMarketShare) + runningSum);
+  });
+  // Accounts for rounding errors, so that the range is always [0, 1].
+  normalizedTiers[normalizedTiers.length - 1] = 1;
+  return normalizedTiers;
 };
+
+
+const random = () => {
+  const normalizedTiers = getNormalizedTiers();
+  const randomNumber = Math.random();
+  // TODO: We should probably at least do bisection here.
+  let index;
+  for (index = 0; index < userAgents.length; index += 1) {
+    if (normalizedTiers[index] >= randomNumber) {
+      break;
+    }
+  }
+  return userAgents[index].userAgent;
+};
+
 
 // eslint-disable-next-line import/prefer-default-export
 export { random };
