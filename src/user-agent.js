d3600202ddd452fc73c706247b0a55d57215f387
Evan Sangaline <esangaline@gmail.com>
2018-08-27 13:12:08 -0400

    Add a first pass of a `UserAgent.filter()` implementation.

--- src/user-agent.js
+++ src/user-agent.js
@@ -1,10 +1,18 @@
 import userAgents from './user-agents.json';
 
 
-class UserAgent extends Function {
-  constructor(filters) {
-    this.currentUserAgentProperties = new Set();
+const defaultWeightIndexPairs = userAgents.map(({ weight }, index) => [weight, index]);
 
+
+export default class UserAgent extends Function {
+  constructor(filters) {
+    super();
+    this.filter(filters);
+    if (this.weightIndexPairs.length === 0) {
+      return null;
+    }
+
+    this.currentUserAgentProperties = new Set();
     this.randomize();
   }
 
@@ -12,11 +20,44 @@ class UserAgent extends Function {
     new UserAgent(filters)
   );
 
+  // This is an internal method, you probably don't want to every call this.
   filter = (filters) => {
     if (!filters) {
       this.userAgents = userAgents;
+      this.weightIndexPairs = defaultWeightIndexPairs;
+      return;
     }
-    // TODO: Apply filters to the list.
+
+    // Turn the various filter formats into a single filter function that acts on raw user agents.
+    let filter;
+    if (typeof filters === 'function') {
+      filter = filters;
+    } else if (typeof filters === 'object') {
+      // TODO: Handle nested properties.
+      filter = (rawUserAgent) => (
+        Object.entries(filters).every(([key, valueFilter]) => {
+          const value = rawUserAgent[key];
+          if (typeof valueFilter === 'function') {
+            return valueFilter(value);
+          }
+          if (valueFilter instanceof RegExp) {
+            return valueFilter.test(value);
+          }
+          return valueFilter === value;
+        })
+      );
+    }
+
+    // Construct normalized weight index pairs given the filters.
+    this.weightIndexPairs = [];
+    let totalWeight = 0;
+    userAgents.forEach((rawUserAgent, index) => {
+      if (filter(rawUserAgent)) {
+        this.weightIndexPairs.push([rawUserAgent.weight, index]);
+        totalWeight += rawUserAgent.weight;
+      }
+    });
+    this.weightIndexPairs = this.weightIndexPairs.map(([weight, index]) => [weight / totalWeight, index]);
   };
 
   random = () => {
