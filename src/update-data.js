bbbf9a53f243ed839de74fee4c05c71fc0f39204
Evan Sangaline <esangaline@gmail.com>
2018-08-27 12:18:07 -0400

    Add definitions of the Google Analytics dimensions that will be queried.

--- src/update-data.js
+++ src/update-data.js
@@ -1,5 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import fs from 'fs';
 
+import jsonStableStringify from 'json-stable-stringify';
+import gaApi from 'ga-api';
+import moment from 'moment';
+import { normal as n } from 'random';
+
+
+// Custom dimensions, see: https://intoli.com/blog/user-agents/
+const customDimensionMap = {
+  'ga:dimension1': 'appName',
+  'ga:dimension2': 'connection',
+  'ga:dimension3': 'cpuClass',
+  'ga:dimension5': 'osCpu',
+  'ga:dimension6': 'platform',
+  'ga:dimension7': 'pluginsLength',
+  'ga:dimension8': 'vendor',
+  'ga:dimension9': 'userAgent',
+};
+// And the special timestamp session ID that we'll use for joining data.
+const sessionIdDimension = 'ga:dimension10';
+
+// Standard dimensions used by Google Analytics.
+const standardDimensionMap = {
+  'ga:browserSize': 'browserSize',
+  'ga:deviceCategory': 'deviceCategory',
+  'ga:screenResolution': 'screenResolution',
+};
 
 
 const getUserAgentTable = () => Promise.resolve();
